473508b0da0d3faf025f47818cee30497a94c593
Daniele Varrazzo <piro@gambitresearch.com>
2015-11-13 23:09:51 +0000

    Read configuration from supervisor config itself, added groups tags

--- suponoff/apps.py
+++ suponoff/apps.py
@@ -3,12 +3,15 @@ from django.conf import settings
 
 import supcast.config
 
+class DummyConf:
+    def items(self):
+        return []
+
 class SupOnOffAppConfig(AppConfig):
     name = 'suponoff'
     verbose_name = "Supervisor On/Off interface"
 
     def ready(self):
-        class DummyConf: pass
         conf = DummyConf()
         conf.redis = settings.SUP_REDIS_URL
         supcast.config.set_config(conf)
